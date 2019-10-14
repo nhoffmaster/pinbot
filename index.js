f0cccc56d3bac36cb27c0e141005bc3f1d66d926
Nate Hoffmaster <nateman1103@gmail.com>
2019-10-13 21:10:05 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const Commando = require("discord.js-commando");
+const bot = new Commando.Client({ partials: ['MESSAGE', 'CHANNEL'] });
+require('dotenv').config()
+
+bot.on("message", (message) => {
+
+});
+
+bot.on('messageReactionAdd', async (reaction, user) => {
+	// When we receive a reaction we check if the message is partial or not
+	if (reaction.message.partial) {
+		// If the message was removed the fetching might result in an API error, which we need to handle
+		try {
+			await reaction.message.fetch();
+		} catch (error) {
+			console.log('Something went wrong when fetching the message: ', error);
+		}
+	}
+	// Now the message has been cached and is fully available
+    if (reaction.emoji.name === '📌') {
+        var channel = bot.channels.get("531575636532396043");
+        channel.send("*\"" + reaction.message.content + "\"* - <@" + user.id + ">\nhttps://ptb.discordapp.com/channels/493512866452996146/" + reaction.message.channel.id + "/" + reaction.message.id);
+    }
+});
+
+bot.on("ready", () => {
+    console.log("Ready");
+});
+
+bot.login(process.env.BOT_TOKEN);

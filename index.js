eb63a8f56a1b3c170b66a85df7d88cddeb99258a
Nate Hoffmaster <nateman1103@gmail.com>
2019-10-16 15:42:44 -0400

    fixed mention error

--- index.js
+++ index.js
@@ -5,7 +5,36 @@ require('dotenv').config()
 bot.on("message", (message) => {
 	if(message.channel.name === "pinboard" && message.author.id != "585542675634192396"){
 		message.delete(1000);
-		message.author.send("You are not PinBot. You may not send messages in #pinboard.")
+		message.author.send("You are not PinBot. You may not send messages in #pinboard.");
+		message.author.send({embed: {
+		    color: 3447003,
+		    author: {
+		      name: bot.user.username,
+		      icon_url: bot.user.avatarURL
+		    },
+		    title: "This is an embed",
+		    url: "http://google.com",
+		    description: "This is a test embed to showcase what they look like and what they can do.",
+		    fields: [{
+		        name: "Fields",
+		        value: "They can have different fields with small headlines."
+		      },
+		      {
+		        name: "Masked links",
+		        value: "You can put [masked links](http://google.com) inside of rich embeds."
+		      },
+		      {
+		        name: "Markdown",
+		        value: "You can put all the *usual* **__Markdown__** inside of them."
+		      }
+		    ],
+		    timestamp: new Date(),
+		    footer: {
+		      icon_url: bot.user.avatarURL,
+		      text: "© Example"
+		    }
+		  }
+		});
 	}
 });
 
@@ -35,7 +64,7 @@ bot.on('messageReactionAdd', async (reaction, user) => {
 			return;
 		}
 
-        channel.send("*\"" + reaction.message.content + "\"* - <@" + user.id + ">\nhttps://ptb.discordapp.com/channels/" + reaction.message.guild.id + "/" + reaction.message.channel.id + "/" + reaction.message.id);
+        channel.send("*\"" + reaction.message.content + "\"* - <@" + message.author.id + ">\nhttps://ptb.discordapp.com/channels/" + reaction.message.guild.id + "/" + reaction.message.channel.id + "/" + reaction.message.id);
     }
 });
 
